600a9859e19df615a9abe304f271d56f69fcc233
trueSlav <2001idercach@gmail.com>
2025-01-29 09:29:00 +0300

    feature(popup): refactor code 3
    Исправлен баг, который перезаписывал значение инпута после закрытия попапа.

--- src/scripts/popup/modal.js
+++ src/scripts/popup/modal.js
@@ -10,18 +10,14 @@ export const setClosePopupEventListeners = popups => {
 }
 
 export const openPopup = popup => {
-	fillInputValueOfEditForm(popup)
 	popup.classList.add('popup_is-opened')
-	document.addEventListener('keydown', e =>
-		handleCloseKey(e, () => closePopup(popup))
-	)
+	document.addEventListener('keydown', handleCloseKey)
 }
 
 export const closePopup = popup => {
 	if (popup.classList.contains('popup_is-opened')) {
 		popup.classList.remove('popup_is-opened')
 		document.removeEventListener('keydown', handleCloseKey)
-		popup.removeEventListener('click', handleCloseOverlay)
 	}
 }
 
@@ -31,20 +27,9 @@ export const handleCloseOverlay = (e, overlay, closePopup) => {
 	}
 }
 
-export const handleCloseKey = (e, close) => {
+export const handleCloseKey = e => {
+	const popup = document.querySelector('.popup_is-opened')
 	if (e.key === 'Escape') {
-		close()
-	}
-}
-
-const fillInputValueOfEditForm = popup => {
-	const nameInput = popup.querySelector('.popup__input_type_name')
-	const jobInput = popup.querySelector('.popup__input_type_description')
-	const profileNameElement = document.querySelector('.profile__title')
-	const profileJobElement = document.querySelector('.profile__description')
-
-	if (nameInput && jobInput && profileNameElement && profileJobElement) {
-		nameInput.value = profileNameElement.textContent
-		jobInput.value = profileJobElement.textContent
+		closePopup(popup)
 	}
 }
