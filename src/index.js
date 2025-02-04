02f3b9583378ccc94fc33ac9402a1cda8595f948
trueSlav <2001idercach@gmail.com>
2025-02-04 22:46:15 +0300

    build(M5 validation): configuring form validation

--- src/index.js
+++ src/index.js
@@ -6,6 +6,16 @@ import {
 	closePopup,
 	openPopup
 } from './scripts/popup/modal'
+import { clearValidation, enableValidation } from './scripts/validation'
+
+const validationOptions = {
+	formSelector: '.popup__form',
+	inputSelector: '.popup__input',
+	submitButtonSelector: '.popup__button',
+	inactiveButtonClass: 'button_inactive',
+	inputErrorClass: 'form__input_type_error',
+	errorClass: 'form__input-error_active'
+}
 
 const openPopupEditButton = document.querySelector('.profile__edit-button')
 const openPopupCreateButton = document.querySelector('.profile__add-button')
@@ -14,34 +24,26 @@ const popupEditElement = document.querySelector('.popup_type_edit')
 const popupCreateElement = document.querySelector('.popup_type_new-card')
 const popups = document.querySelectorAll('.popup')
 
-const openPopupEdit = (popup, openTrigger, form) => {
+// EDIT POPUP
+const openPopupEdit = (popup, openTrigger) => {
+	const formElement = document.querySelector('form[name="edit-profile"]')
+
 	const nameInput = document.querySelector('.popup__input_type_name')
 	const jobInput = document.querySelector('.popup__input_type_description')
+
 	const profileNameElement = document.querySelector('.profile__title')
 	const profileJobElement = document.querySelector('.profile__description')
 
 	const fillInputValueOfEditForm = () => {
-		if (nameInput && jobInput && profileNameElement && profileJobElement) {
-			nameInput.value = profileNameElement.textContent
-			jobInput.value = profileJobElement.textContent
-		}
+		nameInput.value = profileNameElement.textContent
+		jobInput.value = profileJobElement.textContent
 	}
 
 	openTrigger.addEventListener('click', () => {
 		fillInputValueOfEditForm(popup)
 		openPopup(popup)
+		clearValidation(formElement, validationOptions)
 	})
-
-	form(nameInput, jobInput, profileNameElement, profileJobElement)
-}
-
-const createPopupEditForm = (
-	nameInput,
-	jobInput,
-	profileNameElement,
-	profileJobElement
-) => {
-	const formElement = document.querySelector('form[name="edit-profile"]')
 
 	function handleFormSubmit(e) {
 		e.preventDefault()
@@ -57,15 +59,19 @@ const createPopupEditForm = (
 	formElement.addEventListener('submit', handleFormSubmit)
 }
 
+// CREATE POPUP
 const openPopupCreate = (popup, openTrigger, form) => {
-	openTrigger.addEventListener('click', () => openPopup(popup))
-	form()
-}
-
-const createPopupCreateForm = () => {
 	const formElement = document.querySelector('form[name="new-place"]')
 	const nameInput = document.querySelector('.popup__input_type_card-name')
 	const linkInput = document.querySelector('.popup__input_type_url')
+
+	openTrigger.addEventListener('click', () => {
+		if (nameInput.value === '') {
+			const btn = formElement.querySelector('.popup__button')
+			btn.classList.add('button_inactive')
+		}
+		openPopup(popup)
+	})
 
 	function handleFormSubmit(e) {
 		e.preventDefault()
@@ -79,11 +85,13 @@ const createPopupCreateForm = () => {
 		formElement.reset()
 		renderNewCard(data, openPopupImage)
 		closePopup(popupCreateElement)
+		clearValidation(formElement, validationOptions)
 	}
 
 	formElement.addEventListener('submit', handleFormSubmit)
 }
 
+// PICTURE POPUP
 const openPopupImage = (openTrigger, image, caption) => {
 	const popup = document.querySelector('.popup_type_image')
 	const imagePopupElement = popup.querySelector('.popup__image')
@@ -96,15 +104,13 @@ const openPopupImage = (openTrigger, image, caption) => {
 		captionPopupElement.textContent = caption
 	})
 }
-
+// INITIAL
 initialCards.forEach(card => {
 	initialRenderCard(card, openPopupImage)
 })
 
 setClosePopupEventListeners(popups)
-openPopupEdit(popupEditElement, openPopupEditButton, createPopupEditForm)
-openPopupCreate(
-	popupCreateElement,
-	openPopupCreateButton,
-	createPopupCreateForm
-)
+openPopupEdit(popupEditElement, openPopupEditButton)
+openPopupCreate(popupCreateElement, openPopupCreateButton)
+
+enableValidation(validationOptions)
